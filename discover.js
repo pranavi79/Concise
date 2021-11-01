ba7e67d89a1c5a597786e4b80a1c4ea71f294be0
pranavi79 <pranavi.sharma79@gmail.com>
2021-11-01 13:39:25 +0530

    Removed bugs by making maxPage variable global Added lazy loading to discover page

--- discover.js
+++ discover.js
@@ -4,7 +4,8 @@ var discType;
 var discCategory;
 var genres;
 var tokens;
-
+var final_str = "";
+var maxPage=0;
 window.addEventListener('DOMContentLoaded', (event) => {
       // using a loader 
       var results = document.querySelector('.result');
@@ -119,40 +120,59 @@ function displayDiscoverResults(data) {
             noResults();
             return;
       }
-      var final_str = "";
       data.results.forEach(element => {
             final_str += constructHTMLStr(element, discType);
       });
 
-      parent.innerHTML = final_str + getFooter();
-      document.querySelector('footer span').textContent = data.page;
+      parent.innerHTML = final_str;
       // event listeener for details 
       addEventListenerToItems();
-      // adding event listener to the buttons
-      addEventListenerToButtons(data.page, data.total_pages);
-}
-
-function addEventListenerToButtons(currentPage, totalPages) {
-      var prevBtn = document.querySelector('.prev-btn');
-      var nextBtn = document.querySelector('.next-btn');
-      nextBtn.addEventListener('click', function () {
-            if (currentPage < totalPages) {
-                  tokens.pop();
-                  window.location.assign('discover_results.html' + '?' + tokens.join('&') + '&' + (currentPage + 1));
-            } else {
-                  alert("This is the last page!");
-            }
-      });
-      prevBtn.addEventListener('click', function () {
-            if (currentPage >= 2) {
-                  tokens.pop();
-                  window.location.assign('discover_results.html' + '?' + tokens.join('&') + '&' + (currentPage - 1));
-            } else {
-                  alert("This is the first page!");
+      //maxPage=Math.max(maxPage,data.page);
+      window.addEventListener('scroll',()=>{
+            let documentHeight = document.body.scrollHeight;
+            let currentScroll = window.scrollY + window.innerHeight;
+            if(currentScroll+10>documentHeight){
+                  //alert("scrolled to bottom");
+                  if(data.page>maxPage){
+                        maxPage=data.page;
+                        loadMorePages(data.page, data.total_pages);
+                  }
             }
       });
 }
+function loadMorePages(currentPage,totalPages){
+      var loader = document.querySelector('.loader2');
+      if(currentPage < totalPages) {
+            setTimeout(function () {
+            loader.style.display = 'block';
+            if (tokens[0] === "movie") {
+                  if (tokens[1] === "most-liked" || tokens[1] === "highest-grossing") {
+                        // tokens[2] is page number 
+                        tokens[2]=currentPage+1;
+                        discFilters[1] =  options.page(tokens[2]);
+                        discoverMedia("movie", discFilters);
+                  } else {
+                        tokens[3]=currentPage+1;
+                        discFilters[2] = options.page(tokens[3]);
+                        discoverMedia("movie", discFilters);
+                  }
+            } else {
+                  if (tokens[1] === "most-liked") {
+                        tokens[2]=currentPage+1;
+                        discFilters[1]=options.page(tokens[2]);
+                        discoverMedia("tv", discFilters);
+                  } else {
+                        // token[2] becomes the genre list seperated by commas, token[3] - page number
+                        tokens[3]=currentPage+1;
+                        discFilters[2] = options.page(tokens[3]);
+                        discoverMedia("tv", discFilters);
+                  }
+            }}, 1000);
+      } else {
+            loader.style.display = 'none';
+      }
 
+};
 function noResults() {
       document.querySelector('.main-content').innerHTML = '<h1 style="text-align:center"><i style="color: #AFFC41" class="fa fa-frown-o" aria-hidden="true"></i> No results found!</h1>';
 }
--- search.js
+++ search.js
@@ -3,7 +3,7 @@ var genres;
 var QueryStr;
 var searchType;
 var final_str = "";
-var currentPage=0;
+var maxPage=0;
 window.addEventListener('DOMContentLoaded', (event) => {
       // using a loader 
       var results = document.querySelector('.result');
@@ -54,36 +54,36 @@ function displaySearchResults(data) {
       data.results.forEach(element => {
             final_str += constructHTMLStr(element, searchType);
       });
-
       parent.innerHTML=final_str;
       //document.querySelector('footer span').textContent = data.page;
       // event listeener for details 
       addEventListenerToItems(final_str);
       // adding event listener to the buttons
       //addEventListenerToButtons(data.page, data.total_pages);
-      if(data.page>currentPage){
-            currentPage=data.page;
-            window.addEventListener('scroll',()=>{
-                  let documentHeight = document.body.scrollHeight;
-                  let currentScroll = window.scrollY + window.innerHeight;
-                  if(currentScroll+10>documentHeight){
-                        //alert("scrolled to bottom");
-                        loadMorePages(currentPage, data.total_pages);
+      //maxPage=Math.max(maxPage,data.page);
+      window.addEventListener('scroll',()=>{
+            let documentHeight = document.body.scrollHeight;
+            let currentScroll = window.scrollY + window.innerHeight;
+            if(currentScroll+10>documentHeight){
+                  //alert("scrolled to bottom");
+                  if(data.page>maxPage){
+                        maxPage=data.page;
+                        loadMorePages(data.page, data.total_pages);
                   }
-            });
-      }
+            }
+      });
 }
 function loadMorePages(currentPage,totalPages){
-      //console.log(currentPage);
       var loader = document.querySelector('.loader2');
       if(currentPage < totalPages) {
-            var x = setTimeout(function () {
+            setTimeout(function () {
             loader.style.display = 'block';
             searchMedia(searchType,QueryStr,currentPage+1);
       }, 1000);
       } else {
             loader.style.display = 'none';
       }
+
 };
 function noResults() {
       document.querySelector('.main-content').innerHTML = '<h1 style="text-align:center"><i style="color: #AFFC41" class="fa fa-frown-o" aria-hidden="true"></i> No results found!</h1>';
